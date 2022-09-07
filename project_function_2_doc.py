aa789ff6f6d1fe8ec78a72054eaf444c97885f05
heysushil <sushil.chaudhary111@gmail.com>
2022-09-07 21:20:25 +0530

    function project 2

---
+++ project_function_2_doc.py
@@ -0,0 +1,19 @@
+# 1. user ko option show karna hai:
+#     --------------------------------
+#             Let's Start Game
+#     --------------------------------
+#     1. Play
+#     2. Exit 
+#     --------------------------------
+
+# 2. Usper se input lena hai ki wo kya karna chahta hai 
+#     --------------------------------
+#         Choose your lucky number 
+#     --------------------------------
+#     Your number is 
+#     My number is 
+#     --------------------------------
+    
+# 3. Hum user input ko apne number se match karenge, agar number match karta hai to user win otherwise loss 
+
+# 4. Repeat message again and again until user exit. 
